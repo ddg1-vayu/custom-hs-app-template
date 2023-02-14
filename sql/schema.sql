4ad7581651ed1a5cc0a86c3b9c89878c1f6e9c7e
Vayu Dugar <vayu.dugar21@gmail.com>
2023-02-14 18:23:13 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,123 +1,68 @@
+CREATE TABLE `admin_access_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `http_cookie` varchar(512) DEFAULT NULL,
+  `remote_address` varchar(255) NOT NULL,
+  `remote_port` int(11) NOT NULL,
+  `ua_platform` varchar(512) DEFAULT NULL,
+  `ua_version` varchar(512) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `action` varchar(512) NOT NULL,
+  `comment` longtext DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
 CREATE TABLE `api_logs` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`hub_portal_id` int(11) DEFAULT NULL,
-	`api_origin` varchar(255) DEFAULT NULL,
-	`curl_url` text DEFAULT NULL,
-	`curl_payload` longtext DEFAULT NULL,
-	`curl_method` enum(
-		'DELETE',
-		'GET',
-		'HEAD',
-		'OPTIONS',
-		'PATCH',
-		'POST',
-		'PUT'
-	) DEFAULT NULL,
-	`curl_response` longtext DEFAULT NULL,
-	`curl_http_code` int(11) DEFAULT NULL,
-	`curl_type` varchar(255) NOT NULL,
-	`file_name` varchar(255) NOT NULL,
-	`timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hub_portal_id` int(11) DEFAULT 0,
+  `api_origin` varchar(255) DEFAULT NULL,
+  `curl_url` mediumtext DEFAULT NULL,
+  `curl_payload` longtext DEFAULT NULL,
+  `curl_method` enum('GET', 'POST', 'PATCH', 'PUT', 'DELETE') DEFAULT NULL,
+  `curl_response` longtext DEFAULT NULL,
+  `curl_http_code` varchar(50) DEFAULT NULL,
+  `curl_type` varchar(255) DEFAULT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 ROW_FORMAT = DYNAMIC;
 
 CREATE TABLE `app_installs` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`hub_portal_id` int(11) NOT NULL,
-	`hub_timezone` varchar(255) DEFAULT NULL,
-	`install_code` varchar(255) NOT NULL,
-	`refresh_token` varchar(255) NOT NULL,
-	`access_token` varchar(350) NOT NULL,
-	`installed` timestamp NOT NULL DEFAULT current_timestamp(),
-	`last_installed` timestamp NOT NULL DEFAULT current_timestamp(),
-	`token_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`status` enum('Active', 'Inactive') NOT NULL DEFAULT 'Active',
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hub_portal_id` int(11) NOT NULL,
+  `install_code` varchar(255) NOT NULL,
+  `refresh_token` varchar(255) NOT NULL,
+  `access_token` varchar(350) NOT NULL,
+  `installed` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_installed` timestamp NOT NULL DEFAULT current_timestamp(),
+  `token_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` enum('Active', 'Inactive') NOT NULL DEFAULT 'Active',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `billing_details` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`hub_portal_id` int(11) DEFAULT NULL,
-	`first_name` varchar(255) NOT NULL,
-	`last_name` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`phone_number` varchar(16) NOT NULL,
-	`company` varchar(255) NOT NULL,
-	`country` varchar(255) NOT NULL,
-	`subscription_name` enum('Monthly', 'Yearly') NOT NULL DEFAULT 'Monthly',
-	`subscription_amount` varchar(255) DEFAULT NULL,
-	`demo_start` date NOT NULL DEFAULT '0000-00-00',
-	`demo_end` date NOT NULL DEFAULT '0000-00-00',
-	`billing_status` enum('Active', 'Cancelled', 'Inactive', 'Trialing') NOT NULL DEFAULT 'Inactive',
-	`billing_start` date DEFAULT NULL,
-	`billing_end` date DEFAULT NULL,
-	`payment_link` varchar(255) DEFAULT NULL,
-	`customer_id` varchar(255) DEFAULT NULL,
-	`subscription_id` varchar(255) DEFAULT NULL,
-	`billing_email` varchar(255) DEFAULT NULL,
-	`billing_name` varchar(255) DEFAULT NULL,
-	`billing_address` varchar(255) DEFAULT NULL,
-	`billing_city` varchar(255) DEFAULT NULL,
-	`billing_state` varchar(255) DEFAULT NULL,
-	`billing_country` varchar(255) DEFAULT NULL,
-	`billing_postal_code` varchar(16) DEFAULT NULL,
-	`last_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-CREATE TABLE `invoice_details` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`customer_id` varchar(255) NOT NULL,
-	`subscription_id` varchar(255) NOT NULL,
-	`pay_status` varchar(255) NOT NULL DEFAULT 'Paid',
-	`amount_due` varchar(32) DEFAULT NULL,
-	`amount_paid` varchar(32) DEFAULT NULL,
-	`amount_remaining` varchar(32) DEFAULT NULL,
-	`period_start` datetime NOT NULL,
-	`period_end` datetime NOT NULL,
-	`timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `username` varchar(32) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(512) NOT NULL,
+  `added` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `webhooks` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`hub_portal_id` int(11) DEFAULT NULL,
-	`source` varchar(255) NOT NULL,
-	`payload` longtext DEFAULT NULL,
-	`type` varchar(255) DEFAULT NULL,
-	`file` varchar(255) NOT NULL,
-	`status` enum('not processed', 'processed') NOT NULL DEFAULT 'not processed',
-	`timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-	`last_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-CREATE TABLE `registered_users` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`first_name` varchar(255) DEFAULT NULL,
-	`last_name` varchar(255) DEFAULT NULL,
-	`email` varchar(255) NOT NULL,
-	`username` varchar(32) NOT NULL,
-	`password` varchar(512) NOT NULL,
-	`role` varchar(255) DEFAULT NULL,
-	`added` timestamp NOT NULL DEFAULT current_timestamp(),
-	`updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`last_login` timestamp NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-CREATE TABLE `user_access_logs` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`user_id` int(11) NOT NULL,
-	`user` varchar(24) NOT NULL,
-	`http_cookie` varchar(512) DEFAULT NULL,
-	`remote_address` varchar(255) NOT NULL,
-	`remote_port` int(11) NOT NULL,
-	`ua_platform` varchar(255) NOT NULL,
-	`ua_version` varchar(512) DEFAULT NULL,
-	`user_agent` text DEFAULT NULL,
-	`login` timestamp NOT NULL DEFAULT current_timestamp(),
-	`logout` timestamp NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hub_portal_id` int(11) DEFAULT 0,
+  `source` varchar(255) NOT NULL,
+  `payload` longtext DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `status` enum('0', '1') NOT NULL DEFAULT '0' COMMENT '0 - not processed, 1 - processed',
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
