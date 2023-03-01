11a1d12fe836cc6d0236290f584f50d57424ae77
Vayu Dugar <vayu.dugar21@gmail.com>
2023-03-01 17:08:38 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-primary" id="btn-back-to-top" title="Back to Top"><i class="fa fa-chevron-up"></i></button>
+<button type="button" class="btn btn-primary" id="btn-back-to-top" title="Back to Top"><i class="fa-solid fa-chevron-up"></i></button>
 <footer class="mt-auto fw-bold text-center">
-	<span class="text-black"> All Rights Reserved &copy; <?= date("Y"); ?> </span>
+	<span class="text-black"> All Rights Reserved <i class="fa-regular fa-copyright fa-fw"></i> 2023 </span>
 </footer>
