193d827eda9d7344bd7b68aed211b12503b2210a
Vayu Dugar <vayu.dugar21@gmail.com>
2023-03-01 17:16:15 +0530

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -19,6 +19,7 @@ if (isset($_POST['action']) && empty($_POST['action']) == false) {
 			}
 			break;
 		case "get_payload":
+		case "get_payload_json":
 			$output = [];
 
 			$getPayload = mysqli_query($conn, "SELECT `curl_payload` FROM `api_logs` WHERE `id` = '$recordId'");
@@ -49,50 +50,23 @@ if (isset($_POST['action']) && empty($_POST['action']) == false) {
 					$sanitize = preg_replace("/[\r\n]+/", " ", $payload);
 					$output = json_decode(mb_convert_encoding($sanitize, "UTF-8", mb_list_encodings()), true);
 				}
-				print_r($output);
-			} else {
-				echo "null";
-			}
-			break;
-		case "get_payload_json":
-			$output = [];
 
-			$getPayload = mysqli_query($conn, "SELECT `curl_payload` FROM `api_logs` WHERE `id` = '$recordId'");
-			$rows = mysqli_fetch_assoc($getPayload);
-			$payload = $rows['curl_payload'];
-
-			if (is_null($payload) == false && empty($payload) == false) {
-				if (strncmp($payload, "grant_type=refresh_token", 24) === 0) {
-					$array = explode("&", $payload);
-					for ($i = 0; $i < sizeof($array); $i++) {
-						list($key, $value) = explode("=", $array[$i]);
-						$output[$key] = $value;
-					}
-					$output['client_id'] = mask_string($output['client_id'], 12);
-					$output['client_secret'] = mask_string($output['client_secret'], 12);
-					$output['refresh_token'] = mask_string($output['refresh_token'], 12);
-				} elseif (strncmp($payload, "grant_type=authorization_code", 29) === 0) {
-					$array = explode("&", $payload);
-					for ($i = 0; $i < sizeof($array); $i++) {
-						list($key, $value) = explode("=", $array[$i]);
-						$output[$key] = $value;
-					}
-					$output['client_id'] = mask_string($output['client_id'], 12);
-					$output['client_secret'] = mask_string($output['client_secret'], 12);
-					$output['code'] = mask_string($output['code'], 12);
-				} else {
-					$sanitize = preg_replace("/[\r\n]+/", " ", $payload);
-					$output = json_decode(mb_convert_encoding($sanitize, "UTF-8", mb_list_encodings()), true);
+				if ($action == "get_payload") {
+					print_r($output);
+				} elseif ($action == "get_payload_json") {
+					echo json_encode($output, JSON_PRETTY_PRINT);
 				}
-				echo json_encode($output, JSON_PRETTY_PRINT);
 			} else {
 				echo "null";
 			}
 			break;
 		case "get_response":
+		case "get_response_json":
 			$getResponse = mysqli_query($conn, "SELECT `curl_response` FROM `api_logs` WHERE `id` = '$recordId'");
 			$rows = mysqli_fetch_assoc($getResponse);
 			$response = $rows['curl_response'];
+
+			$output = [];
 
 			if (empty($response) == false && $response != "null" && $response != NULL) {
 				if (strncmp($response, "<html>", 6) === 0 || strncmp($response, "<!DOCTYPE", 8) === 0) {
@@ -105,9 +79,15 @@ if (isset($_POST['action']) && empty($_POST['action']) == false) {
 						$token_arr = $dec_response;
 						$token_arr['refresh_token'] = mask_string($token_arr['refresh_token'], 12);
 						$token_arr['access_token'] = mask_string($token_arr['access_token'], 60);
-						print_r($token_arr);
+						$output = $token_arr;
 					} else {
-						print_r($dec_response);
+						$output = $dec_response;
+					}
+
+					if ($action == "get_response") {
+						print_r($output);
+					} elseif ($action == "get_response_json") {
+						echo json_encode($output, JSON_PRETTY_PRINT);
 					}
 				}
 			} else {
@@ -115,17 +95,6 @@ if (isset($_POST['action']) && empty($_POST['action']) == false) {
 			}
 			break;
 		case "get_webhook":
-			$getWebhook = mysqli_query($conn, "SELECT `payload` FROM `webhooks` WHERE `id` = '$recordId'");
-			if (mysqli_num_rows($getWebhook) > 0) {
-				$webhookData = mysqli_fetch_assoc($getWebhook);
-				$response = $webhookData['payload'];
-				$sanitize = preg_replace("/[\r\n]+/", " ", $response);
-				$dec_response = json_decode(mb_convert_encoding($sanitize, "UTF-8", mb_list_encodings()), true);
-				print_r($dec_response);
-			} else {
-				echo "null";
-			}
-			break;
 		case "get_webhook_json":
 			$getWebhook = mysqli_query($conn, "SELECT `payload` FROM `webhooks` WHERE `id` = '$recordId'");
 			if (mysqli_num_rows($getWebhook) > 0) {
@@ -133,7 +102,11 @@ if (isset($_POST['action']) && empty($_POST['action']) == false) {
 				$response = $webhookData['payload'];
 				$sanitize = preg_replace("/[\r\n]+/", " ", $response);
 				$dec_response = json_decode(mb_convert_encoding($sanitize, "UTF-8", mb_list_encodings()), true);
-				echo json_encode($dec_response, JSON_PRETTY_PRINT);
+				if ($action == "get_webhook") {
+					print_r($dec_response);
+				} elseif ($action == "get_webhook_json") {
+					echo json_encode($dec_response, JSON_PRETTY_PRINT);
+				}
 			} else {
 				echo "null";
 			}
