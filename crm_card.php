47b3f98a7c8aef88b953706d9b0bd802bd01b11f
Vayu Dugar <vayu.dugar21@gmail.com>
2023-04-12 18:47:27 +0530

    functions added & added signature verification to crm_card

--- crm_card.php
+++ crm_card.php
@@ -1,34 +1,79 @@
 <?php
-$portalId = empty($_GET['portalId']) ? NULL : $_GET['portalId'];
-$objectId = empty($_GET['associatedObjectId']) ? NULL : $_GET['associatedObjectId'];
-$userId = empty($_GET['userId']) ? NULL : $_GET['userId'];
-$userEmail = empty($_GET['userEmail']) ? NULL : $_GET['userEmail'];
+include("globals.php");
 
-$link = "";
-$frameLink = "";
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$unixTimestamp = $_SERVER['HTTP_X_HUBSPOT_REQUEST_TIMESTAMP'];
+$url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$version = $_SERVER['HTTP_X_HUBSPOT_SIGNATURE_VERSION'];
+$signature = $_SERVER['HTTP_X_HUBSPOT_SIGNATURE'];
 
-$payload = [
-	"results" => [
-		[
-			"objectId" => $objectId,
-			"title" => "CRM Card",
-			"link" => $link,
-			"properties" => [
-				[
-					"label" => "Sample Card",
-					"dataType" => "STRING",
-					"value" => "This is a sample"
+switch ($version) {
+	case "v1":
+		$data = $clientSecret;
+		if (isset($webhook) && empty($webhook) == false) {
+			$data = $data . $webhook;
+		}
+		$hashData = hash("sha256", $data);
+		break;
+	case "v2":
+		$data = $clientSecret . $httpMethod . $url;
+		if (isset($webhook) && empty($webhook) == false) {
+			$data = $data . $webhook;
+		}
+		$hashData = hash("sha256", $data);
+		break;
+	case "v3":
+		$timestamp = floor($unixTimestamp / 1000);
+		$currentTimestamp = time();
+		$difference = ($currentTimestamp - $timestamp);
+		$httpMethod =  "POST";
+		if ($difference > 300) {
+			$data = $httpMethod . $url . $webhook . $unixTimestamp;
+			$encData = mb_convert_encoding($data, "UTF-8", mb_list_encodings());
+			$hashData = base64_encode(hash_hmac("sha256", $encData, $clientSecret, false));
+		} else {
+			echo "Hubspot's v3 Signature has expired! The request cannot be processed.";
+		}
+		break;
+	default:
+		die("Signature ERROR");
+		break;
+}
+
+if ($signature == $hashData) {
+	$objectId = (isset($_GET['associatedObjectId']) && empty($_GET['associatedObjectId'] == false)) ? strip_tags(trim($_GET['associatedObjectId'])) : NULL;
+	$portalId = (isset($_GET['portalId']) && empty($_GET['portalId'] == false)) ? strip_tags(trim($_GET['portalId'])) : NULL;
+	$userEmail = (isset($_GET['userEmail']) && empty($_GET['userEmail'] == false)) ? strip_tags(trim($_GET['userEmail'])) : NULL;
+	$userId = (isset($_GET['userId']) && empty($_GET['userId'] == false)) ? strip_tags(trim($_GET['userId'])) : NULL;
+
+	$link = "";
+	$frameLink = "";
+
+	$payload = [
+		"results" => [
+			[
+				"objectId" => $objectId,
+				"title" => "CRM Card",
+				"link" => $link,
+				"properties" => [
+					[
+						"label" => "Sample Card",
+						"dataType" => "STRING",
+						"value" => "This is a sample"
+					]
 				]
 			]
+		],
+		"primaryAction" => [
+			"type" => "IFRAME",
+			"width" => 640,
+			"height" => 480,
+			"uri" => $frameLink,
+			"label" => "View"
 		]
-	],
-	"primaryAction" => [
-		"type" => "IFRAME",
-		"width" => 640,
-		"height" => 480,
-		"uri" => $frameLink,
-		"label" => "View"
-	]
-];
+	];
 
-echo json_encode($payload);
+	echo json_encode($payload);
+
+	header("Content-Type: application/json");
+}
--- functions.php
+++ functions.php
@@ -1,64 +1,6 @@
 <?php
 $successCodes = [200, 201, 202, 203, 204, 205];
 
-/* ------------------------ cURL FUNCTIONS ------------------------ */
-	/**
-	 * Make a GET/DELETE cURL Request
-	 *
-	 * @param string $endpoint
-	 * @param array $customHeaders
-	 * @param string $method
-	 * @return array $result
-	 */
-	function cURL_getRequest($endpoint, $customHeaders, $method) {
-		$curlQuery = curl_init();
-		curl_setopt($curlQuery, CURLOPT_URL, $endpoint);
-		curl_setopt($curlQuery, CURLOPT_HTTP_VERSION, "CURL_HTTP_VERSION_1_1");
-		curl_setopt($curlQuery, CURLOPT_HTTPHEADER, $customHeaders);
-		curl_setopt($curlQuery, CURLOPT_CUSTOMREQUEST, $method);
-		curl_setopt($curlQuery, CURLOPT_RETURNTRANSFER, true);
-		$response = curl_exec($curlQuery);
-		$httpCode = curl_getinfo($curlQuery, CURLINFO_RESPONSE_CODE);
-		$result = [
-			"httpCode" => $httpCode,
-			"response" => $response
-		];
-
-		curl_close($curlQuery);
-
-		return $result;
-	}
-
-	/**
-	 * Make a POST/PATCH cURL request
-	 *
-	 * @param string $endpoint
-	 * @param array $customHeaders
-	 * @param string $method
-	 * @param string $payload
-	 * @return array $result
-	 */
-	function cURL_request($endpoint, $customHeaders, $method, $payload) {
-		$curlQuery = curl_init();
-		curl_setopt($curlQuery, CURLOPT_URL, $endpoint);
-		curl_setopt($curlQuery, CURLOPT_HTTP_VERSION, "CURL_HTTP_VERSION_1_1");
-		curl_setopt($curlQuery, CURLOPT_HTTPHEADER, $customHeaders);
-		curl_setopt($curlQuery, CURLOPT_CUSTOMREQUEST, $method);
-		curl_setopt($curlQuery, CURLOPT_POSTFIELDS, $payload);
-		curl_setopt($curlQuery, CURLOPT_RETURNTRANSFER, true);
-		$response = curl_exec($curlQuery);
-		$httpCode = curl_getinfo($curlQuery, CURLINFO_RESPONSE_CODE);
-		$result = [
-			"httpCode" => $httpCode,
-			"response" => $response
-		];
-
-		curl_close($curlQuery);
-
-		return $result;
-	}
-/* ------------------------ END ------------------------ */
-
 /* ------------------------ DATABASE FUNCTIONS ------------------------ */
 	/**
 	 * Fetch Access Token from Database
@@ -72,17 +14,6 @@ $successCodes = [200, 201, 202, 203, 204, 205];
 		$tokenRow = mysqli_fetch_array($getToken);
 		$access_token = $tokenRow['access_token'];
 		return $access_token;
-	}
-
-	/**
-	 * Update processing status of webhook
-	 *
-	 * @param int $id
-	 * @return void
-	 */
-	function webhookStatusUpdate($id) {
-		global $conn;
-		mysqli_query($conn, "UPDATE `webhooks` SET `status` = '1' WHERE `id` = '$id'");
 	}
 
 	/**
@@ -121,9 +52,118 @@ $successCodes = [200, 201, 202, 203, 204, 205];
 		global $conn;
 		mysqli_query($conn, "INSERT INTO `api_logs` (`hub_portal_id`, `api_origin`, `curl_url`, `curl_payload`, `curl_method`, `curl_response`, `curl_http_code`, `curl_type`, `file_name`) VALUES ('$portal', '$origin', '" . addslashes($endpoint) . "', '" . addslashes($payload) . "', '$method', '" . addslashes($response) . "', '$httpCode', '$type', '" . addslashes($fileName) . "')");
 	}
+
+	/**
+	 * Insert the data received from a HTTP Request in the DB
+	 *
+	 * @param int $portalId
+	 * @param string $source
+	 * @param string $payload
+	 * @param string $type
+	 * @param string $fileName
+	 * @return void
+	 */
+	function webhookInsert($portalId, $source, $payload, $type, $fileName) {
+		global $conn;
+		mysqli_query($conn, "INSERT INTO `webhooks` (`hub_portal_id`, `source`, `payload`, `type`, `file_name`) VALUES ('$portalId', '$source', '$payload', '$type', '$fileName')");
+	}
+
+	/**
+	 * Mark a webhook as Processed
+	 *
+	 * @param int $id
+	 * @return void
+	 */
+	function webhookStatusUpdate($id) {
+		global $conn;
+		mysqli_query($conn, "UPDATE `webhooks` SET `status` = '1' WHERE `id` = '$id'");
+	}
 /* ------------------------ END ------------------------ */
 
-/* ------------------------ HUBSPOT CONTACT FUNCTIONS ------------------------ */
+/* ------------------------ HUBSPOT FUNCTIONS ------------------------ */
+	/**
+	 * Get HubSpot Account Info by Access Token
+	 *
+	 * @param string $appName
+	 * @param string $accessToken
+	 * @param string $fileName
+	 * @param int $try
+	 * @return array
+	 */
+	function hsAccountInfo($accessToken, $fileName, $try = 0) {
+		global $successCodes;
+
+		$method = "GET";
+		$origin = "HubSpot";
+		$type = "Account Info";
+
+		$endpoint = "https://api.hubapi.com/account-info/v3/details";
+
+		$customHeaders = ["Authorization: Bearer $accessToken", "Content-type: application/json"];
+
+		$result = cURL_getRequest($endpoint, $customHeaders, $method);
+		$response = $result['response'];
+		$httpCode = $result['httpCode'];
+
+		$responseArr = json_decode($response, true);
+		$portalId = $responseArr['portalId'];
+
+		log_get_request($portalId, $origin, $endpoint, $method, $response, $httpCode, $type, $fileName);
+
+		if (in_array($httpCode, $successCodes) == false && $try < 4) {
+			$try++;
+			sleep(2);
+			hsAccountInfo($accessToken, $fileName, $try);
+		} else {
+			$resultArr = (isset($responseArr) && empty($responseArr) == false) ? $responseArr : [];
+		}
+
+		return $resultArr;
+	}
+
+	/**
+	 * Retreive all user of the provided HubSpot A/c
+	 *
+	 * @param string $appName
+	 * @param int $portalId
+	 * @param string $accessToken
+	 * @param string $fileName
+	 * @param array $list
+	 * @param int $limit
+	 * @param string $after
+	 * @param int $try
+	 * @return array
+	 */
+	function hsAccountOwners($portalId, $accessToken, $fileName, $list = [], $limit = 100, $after = "", $try = 0) {
+		$method = "GET";
+		$origin = "HubSpot";
+		$type = "Account Owners";
+
+		$after = ($after != "") ? "after=$after&" : "";
+
+		$endpoint = "https://api.hubapi.com/crm/v3/owners/?archived=false&limit=$limit&$after";
+
+		$customHeaders = ["Content-Type: application/json", "Authorization: Bearer $accessToken"];
+
+		$result = cURL_getRequest($endpoint, $customHeaders, $method);
+		$response = $result['response'];
+		$httpCode = $result['httpCode'];
+
+		log_get_request($portalId, $origin, $endpoint, $method, $response, $httpCode, $type, $fileName);
+
+		$responseArr = json_decode($response, true);
+		if (isset($responseArr['results']) && !empty($responseArr['results'])) {
+			$list = array_merge($list, $responseArr['results']);
+
+			if (isset($responseArr['paging']['next']['after']) && $responseArr['paging']['next']['after'] != '') {
+				$new_after = $responseArr['paging']['next']['after'];
+				return hsAccountOwners($portalId, $accessToken, $fileName, $list, $limit = 1, $new_after);
+			}
+		}
+
+		return $list;
+	}
+
 	/**
 	 * Search HubSpot for Contact with provided filters
 	 *
@@ -311,9 +351,36 @@ $successCodes = [200, 201, 202, 203, 204, 205];
 	
 		return $result;
 	}
-/* ------------------------ END ------------------------ */
 
-/* ------------------------ HUBSPOT CUSTOM ACTION FUNCTIONS ------------------------ */
+	/**
+	 * Create a Timeline Event on HubSpot
+	 *
+	 * @param string $appName
+	 * @param int $portalId
+	 * @param string $accessToken
+	 * @param string $eventType
+	 * @param string $eventData
+	 * @param string $fileName
+	 * @return int
+	 */
+	function hsTimelineEvent($portalId, $accessToken, $eventType, $eventData, $fileName) {
+		$origin = "HubSpot";
+		$method = "POST";
+
+		$endpoint = "https://api.hubapi.com/crm/v3/timeline/events";
+
+		$customHeaders = ["Authorization: Bearer $accessToken", "Content-type: application/json"];
+
+		$payload = json_encode($eventData);
+		$result = cURL_request($endpoint, $customHeaders, $method, $payload);
+		$response = $result['response'];
+		$httpCode = $result['httpCode'];
+
+		log_request($portalId, $origin, $endpoint, $payload, $method, $response, $httpCode, $eventType, $fileName);
+
+		return $httpCode;
+	}
+
 	/**
 	 * Get all custom workflow actions for the provided App ID
 	 *
@@ -436,6 +503,132 @@ $successCodes = [200, 201, 202, 203, 204, 205];
 /* ------------------------ END ------------------------ */
 
 /* ------------------------ OTHER FUNCTIONS ------------------------ */
+	/**
+	 * Make a GET/DELETE cURL Request
+	 *
+	 * @param string $endpoint
+	 * @param array $customHeaders
+	 * @param string $method
+	 * @return array $result
+	 */
+	function cURL_getRequest($endpoint, $customHeaders, $method) {
+		$curlQuery = curl_init();
+		curl_setopt($curlQuery, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($curlQuery, CURLOPT_ENCODING, "");
+		curl_setopt($curlQuery, CURLOPT_HTTPHEADER, $customHeaders);
+		curl_setopt($curlQuery, CURLOPT_HTTP_VERSION, "CURL_HTTP_VERSION_1_1");
+		curl_setopt($curlQuery, CURLOPT_MAXREDIRS, 10);
+		curl_setopt($curlQuery, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlQuery, CURLOPT_TIMEOUT, 30);
+		curl_setopt($curlQuery, CURLOPT_URL, $endpoint);
+		$response = curl_exec($curlQuery);
+		$httpCode = curl_getinfo($curlQuery, CURLINFO_RESPONSE_CODE);
+		$resultArr = [
+			"httpCode" => $httpCode,
+			"response" => $response
+		];
+
+		curl_close($curlQuery);
+
+		return $resultArr;
+	}
+
+	/**
+	 * Make a POST/PATCH cURL request
+	 *
+	 * @param string $endpoint
+	 * @param array $customHeaders
+	 * @param string $method
+	 * @param string $payload
+	 * @return array $result
+	 */
+	function cURL_request($endpoint, $customHeaders, $method, $payload) {
+		$curlQuery = curl_init();
+		curl_setopt($curlQuery, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($curlQuery, CURLOPT_ENCODING, "");
+		curl_setopt($curlQuery, CURLOPT_HTTPHEADER, $customHeaders);
+		curl_setopt($curlQuery, CURLOPT_HTTP_VERSION, "CURL_HTTP_VERSION_1_1");
+		curl_setopt($curlQuery, CURLOPT_MAXREDIRS, 10);
+		curl_setopt($curlQuery, CURLOPT_POSTFIELDS, $payload);
+		curl_setopt($curlQuery, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlQuery, CURLOPT_TIMEOUT, 30);
+		curl_setopt($curlQuery, CURLOPT_URL, $endpoint);
+		$response = curl_exec($curlQuery);
+		$httpCode = curl_getinfo($curlQuery, CURLINFO_RESPONSE_CODE);
+		$resultArr = [
+			"httpCode" => $httpCode,
+			"response" => $response
+		];
+
+		curl_close($curlQuery);
+
+		return $resultArr;
+	}
+
+	/**
+	 * Return the file type of the provided URL
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	function getFileType($url) {
+		// Use a try block to catch any exceptions
+		try {
+			// Parse the URL and get the path component
+			$path = parse_url($url, PHP_URL_PATH);
+			// Check if the path has a file extension
+			if (pathinfo($path, PATHINFO_EXTENSION)) {
+				// Return the file extension
+				return pathinfo($path, PATHINFO_EXTENSION);
+			} else {
+				// Perform a HEAD request to the URL and get the content type header
+				$headers = get_headers($url, 1);
+				// Check if the headers are valid
+				if ($headers !== false) {
+					$content_type = $headers['Content-Type'];
+					// Check if the content type is a valid MIME type
+					if (preg_match('/^[\w-]+\/[\w-]+$/', $content_type)) {
+						// Return the MIME type
+						return $content_type;
+					} else {
+						// Throw an exception for invalid MIME type
+						throw new Exception("Invalid MIME type: $content_type");
+					}
+				} else {
+					// Throw an exception for failed request
+					throw new Exception("Failed to get headers for: $url");
+				}
+			}
+		} catch (Exception $e) {
+			// Catch any exceptions and return an error message
+			return "Error: " . $e->getMessage();
+		}
+	}
+	
+	/**
+	 * Make HTML Anchors for all HTTPS urls present in the provided string
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	function linkify($string)	{
+		$pattern = '/(https?:\/\/[^\s]+)/i';
+		$replacement = '<a target="_blank" href="$1">$1</a>';
+		return preg_replace($pattern, $replacement, $string);
+	}
+	
+	/**
+	 * Make HTML Anchors for all email addresses present in the provided string
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	function linkifyEmail($string)	{
+		$pattern = '/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/';
+		$replacement = '<a href="mailto:$1" target="_blank">$1</a>';
+		return preg_replace($pattern, $replacement, $string);
+	}
+
 	/**
 	 * Display either the error generated by the cURL request or the success message
 	 *
